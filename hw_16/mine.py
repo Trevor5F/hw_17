c6c6fb3ff08a6637168a269f504aa64d47ec5356
Admin <Dexter2366@yandex.ru>
2022-05-11 10:31:49 +0300

    Добавил app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True

--- hw_16/mine.py
+++ hw_16/mine.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 db = SQLAlchemy(app)
 
 
